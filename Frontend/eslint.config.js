d48000ecf7865030b71cdb2b996e016fe118e410
Rebeler_PC <1305761606@qq.com>
2026-05-07 18:21:38 +0800

    chore (config): 配置 ESLint TypeScript 规则
    - 添加 @typescript-eslint 插件
    - 禁用原生 no-unused-vars 规则
    - 配置 TypeScript 未使用变量检测规则
    - 添加变量名忽略模式（以下划线开头）

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -9,6 +9,18 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error", {
+        "argsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }]
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
