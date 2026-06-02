201f86d12d241e14ec311fbf59440e97ca985cc7
Rebeler_PC <1305761606@qq.com>
2026-06-03 02:26:02 +0800

    style (BlogListHero) : 调整样式

--- Frontend/src/components/Sections/BlogList/BlogListHero.tsx
+++ Frontend/src/components/Sections/BlogList/BlogListHero.tsx
@@ -4,7 +4,7 @@ const BlogListHero = () => {
   return (
     <div className="w-full bg-dot-pattern h-60 flex flex-col justify-center">
       <div className="w-full max-w-7xl mx-auto px-4">
-        <h1 className="text-4xl font-bold mb-2">博客文章</h1>
+        <h1 className="text-5xl font-bold mb-2">博客文章</h1>
         <p className="text-gray-600">分享技术心得、项目经验与编程感悟</p>
       </div>
     </div>
